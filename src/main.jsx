2f73cef247dc6c1e24cc64e0f7dc8f9dd5779878
nexoventlabs-official <nexoventlabs@gmail.com>
2026-04-24 18:59:02 +0530

    Add loading animation and preload critical images

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const hidePreloader = () => {
+  const el = document.getElementById('preloader')
+  if (el) {
+    el.classList.add('hide')
+    setTimeout(() => el.remove(), 600)
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+window.addEventListener('load', hidePreloader)
